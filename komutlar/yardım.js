8ec3fe0a7f582174e25ea62d980c7d784cba7ade
mustafamert1011 <58435757+mustafamert1011@users.noreply.github.com>
2021-04-15 12:51:22 +0300

    Update yardım.js

--- komutlar/yardım.js
+++ komutlar/yardım.js
@@ -1,51 +1,57 @@
-const Discord = require('discord.js');
-const db = require('quick.db');
-const ayarlar = require('../ayarlar.json');
-const talkedRecently = new Set();
-let botid = ('709489466913325168') 
- 
-exports.run = async(client, message, args) => { 
-  let prefix = (await db.fetch(`prefix_${message.guild.id}`)) || "!";
+const Discord = require("discord.js");
 
-    const embed = new Discord.MessageEmbed()
-        .setAuthor(`${client.user.username} `, client.user.displayAvatarURL({dynamic: true}))
-        .setColor('#d02090')
-       .setTitle(` **Narcos Bot Yardım Menüsüne Hoşgeldiniz** `)
-        .setDescription(`
-  **<a:ok:782623356301213747>  ${prefix}alev**  alevli logo yapar
-  **<a:ok:782623356301213747>  ${prefix}alev2**  alevli logo yapar 
-  **<a:ok:782623356301213747>  ${prefix}gold **  goldlu logo yapar
-  **<a:ok:782623356301213747>  ${prefix}gökkuşak**  gökkuşak logo yapar
-  **<a:ok:782623356301213747>  ${prefix}neon**  neon logo yapar
-  **<a:ok:782623356301213747>  ${prefix}pembe**  pembe logo yapar
-  **<a:ok:782623356301213747>  ${prefix}altın **  altın logo yapar
-  **<a:ok:782623356301213747>  ${prefix}anime**  anime logo yapar
-  **<a:ok:782623356301213747>  ${prefix}google**  google logo yapar
-  **<a:ok:782623356301213747>  ${prefix}odun**  odun logo yapar
-▬▬▬▬▬▬▬▬ \`\`\Genel Komutlar\`\`\ ▬▬▬▬▬▬▬▬
+exports.run = (client, message) => {
+  const EmbedNarcosCode = new Discord.MessageEmbed()
 
-**<a:d_:782623868660482049>  ${prefix}davet __Botu Davet Edebilirsiniz!__**
+    .setColor("RANDOM")
+    .setTitle(
+      "**▬▬▬▬▬▬[** ©️ **» Narcos Bot** ©️ **]▬▬▬▬▬▬**  **  **Prefixim: `!`"
+    )
+    .setThumbnail(
+      "https://cdn.discordapp.com/emojis/770265448892858368.gif?v=1"
+    )
+    .setDescription(
+      `
 
+**▬▬▬▬▬▬▬[** :closed_lock_with_key:  **Komut Listesi**   :closed_lock_with_key: **]▬▬▬▬▬▬**
 
+  **<a:ok:782623356301213747>  !alev**  alevli logo yapar
+  **<a:ok:782623356301213747>  !alev2**  alevli logo yapar 
+  **<a:ok:782623356301213747>  !gold **  goldlu logo yapar
+  **<a:ok:782623356301213747>  !gökkuşak**  gökkuşak logo yapar
+  **<a:ok:782623356301213747>  !neon**  neon logo yapar
+  **<a:ok:782623356301213747>  !pembe**  pembe logo yapar
+  **<a:ok:782623356301213747>  !altın **  altın logo yapar
+  **<a:ok:782623356301213747>  !anime**  anime logo yapar
+  **<a:ok:782623356301213747>  !google**  google logo yapar
+  **<a:ok:782623356301213747>  !odun**  odun logo yapar
 
-`)
-        .setThumbnail(`https://cdn.discordapp.com/attachments/782622358925213708/782626126891581470/584d07d8803af1e79f1c468cbbfc7f52.gif`)
-        .addField(`» Narcos Bot Bağlantıları`, ` <:771730904325816350:782622833221304331>  [Bot Davet Linki](https://discord.com/oauth2/authorize?client_id=645226005144797184&scope=bot&permissions=805314622) **|** [Destek Sunucusu](https://discord.gg/NMnPUjK) **|** <:771730904325816350:782622833221304331> `)
-        .setFooter(`${message.author.username} Tarafından İstendi.`, message.author.displayAvatarURL({dynamic: true}))
-    return  message.channel.send(embed);
-  
-  
+**▬▬▬▬▬▬▬[** :gear:  **Bilgilendirme**  :gear: **]▬▬▬▬▬▬**
+
+> :dizzy: **Fikirleriniz** **değerlidir, Belirtmekten asla çekinmeyin!**
+> :open_file_folder: **Botun Destek Sunucusuna Gelmek İçin [Tıkla!](https://discord.gg/ZBmqym9JhG)**
+> :round_pushpin: **Komut hakkında detaylı bilgi için: !yardım**
+> :bulb: **Discord Js Sürümü: 12.4.0**
+
+**» Bağlantılar** 
+**[Destek Sunucusu](https://discord.gg/ZBmqym9JhG)** **•** **[Botun Davet Linki](https://discord.gg/ZBmqym9JhG)** **•** **[Web-Site]( https://discord.gg/ZBmqym9JhG)**
+`
+    )
+
+    .setFooter(client.user.username + "", client.user.avatarURL)
+    .setTimestamp();
+
+  return message.channel.send(EmbedNarcosCode).then;
 };
-
 exports.conf = {
   enabled: true,
   guildOnly: false,
-  aliases: ['yardım'],
-  permLevel: 0,
+  aliases: [],
+  permLevel: 0
 };
 
 exports.help = {
-  name: 'yardım',
-  description: 'a!davet-sistemi Menüsü',
-  usage: 'yardım'
+  name: "yardım",
+  description: "Botun Komut Listesini Gösterir!",
+  usage: "-yardım"
 };
